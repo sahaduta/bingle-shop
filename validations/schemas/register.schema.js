d104d3caf33d6569f9875ffb900580b4477003ae
sahaduta <sahadutategar@gmail.com>
2023-02-05 13:08:24 +0700

    add validation for address and phone

--- validations/schemas/register.schema.js
+++ validations/schemas/register.schema.js
@@ -3,7 +3,9 @@ const Joi = require('joi');
 const registerSchema = Joi.object({
     email: Joi.string().email().required().min(5),
     password: Joi.string().min(8).required(),
-    name: Joi.string().min(3).required()
+    name: Joi.string().min(3).required(),
+    address: Joi.string().optional().allow(''),
+    phone: Joi.string().optional().allow('')
 }).required();
 
 module.exports = {registerSchema};
